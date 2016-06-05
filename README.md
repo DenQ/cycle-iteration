42383ebd7891289d7d535f59207655b6433714a4
DenQ <denquick@gmail.com>
2016-06-05 19:07:56 +0300

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cycle-iteration
 JavaScript library for cycle iteration array, collection, etc...
+
+```
+var mas = [1, 2, 3];
+var ci = new CycleIterator(mas);
+
+console.log(ci.next());     //1
+console.log(ci.next());     //2
+console.log(ci.next());     //3
+console.log(ci.next());     //1
+console.log(ci.next());     //2
+```
---
+++ dist/cycle-iterator.npm.js
@@ -0,0 +1,22 @@
+var CycleIterator = function(mas) {
+    this.mas = mas;
+    this.generator = this._next();
+};
+
+CycleIterator.prototype._next = function* _next() {
+    var i = 0;
+    while(true) {
+        if (i < this.mas.length) {
+            i++;
+            yield this.mas[i-1];
+        } else {
+            i = 0;
+        }
+    }
+};
+
+CycleIterator.prototype.next = function() {
+    return this.generator.next().value;
+};
+
+module.exports = CycleIterator;
